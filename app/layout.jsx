58d6bf499396eab0b507d0b9e881e46f61a2364b
93amrkakashi <93amr.kakashi@gmail.com>
2025-04-08 22:46:54 +0200

    theme-color test

--- app/layout.jsx
+++ app/layout.jsx
@@ -15,13 +15,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className={` `}>
-      <body
-        className={` `}
-        // style={{
-        //   backgroundColor: "#F3F4F6", // خلفية فاتحة
-        //   darkMode: { backgroundColor: "#1E1F1D" }, // خلفية الوضع الليلي
-        // }}
-      >
+      <head>
+        <meta name="theme-color" content="#49B42B" />
+      </head>
+
+      <body className={` `}>
         <AppProviders>
           <Wraper>{children}</Wraper>
         </AppProviders>
